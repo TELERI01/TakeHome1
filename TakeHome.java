4c852c538e9a49761ba7ae32a80500821fd86f9f
TELERI01 <112411816+TELERI01@users.noreply.github.com>
2022-09-07 11:54:25 -0400

    Add files via upload

---
+++ TakeHome.java
@@ -0,0 +1,18 @@
+public class TakeHome
+{
+
+public static void main(String args[])
+{
+
+System.out.println("My First Name is: " + args[0]);
+System.out.println("My Last Name is: " + args[1]);
+System.out.println("My Hobbies are: " + args[2]);
+System.out.println("My Major is: " + args[3]);
+
+
+}
+
+
+
+
+}
